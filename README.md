958fa169b703ca5844d93c4b38c3c8012e64c146
deyan petrov <deyanp19@hotmail.com>
2018-06-06 17:14:57 -0500

    final touches

--- README.md
+++ README.md
@@ -3,3 +3,11 @@ search assistant
 // The purpose of this app is to create a searchable capability by connecting to twitter, spotify and OMDB
 // I will create NodeJS environment wihich will get all the dynamic requests
 
+// 1. I created an environment by installing the pakcages and initializing the node 
+// 2. I installed different packages needed to the envirinment- require, tweeter, spotify
+//3. Created .gitignore files so the whole environment of node is not uploaded to github
+//4. Created variables in which the keys to call the tweeter and spotify API are stored
+//5. Created .evo file wiich holds the keys itself so they are not vidsible to everybody who visits the site
+//
+ 
+ 
--- liri.js
+++ liri.js
@@ -1,176 +1,91 @@
-// require("dotenv").config();
-
-
-
-// // NPM - Packages && Required files
-// var Twitter = require('twitter');
-// var Spotify = require('node-spotify-api');
-// var request = require('request');
-// var terminalLink = require('terminal-link');
-
-// var fs = require("fs");
-// var keys = require("./keys.js");// links to the keys file (9)
-// var data = fs.readFileSync("./random.txt", "utf8");
-
-
-// var spotify = new Spotify(keys.spotify);
-// var client = new Twitter(keys.twitter);
-
 require("dotenv").config();
+
+
 
 // NPM - Packages && Required files
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var request = require('request');
-var terminalLink = require('terminal-link');
+//var terminalLink = require('terminal-link');
 
 var fs = require("fs");
-var keys = require("./keys.js");
+var keys = require("./keys.js");// links to the keys file (9)
 var data = fs.readFileSync("./random.txt", "utf8");
 
-// Keys
-var spotify = new Spotify(keys.spotify);
+var command = process.argv[2];
+
+//Keys for twitter and spotify
+var spotify = new Spotify(keys.spotify); //create new constructor
 var client = new Twitter(keys.twitter);
 
-// Logs userinput
-var search = process.argv[2];
-var entireCmdLine = process.argv;
-var findThis = "";
+switch (command) {
+    case 'movie':
+        movie();
+        break;
 
-// Text colors
-var FgBlue = "\x1b[34m";
-var FgWhite = "\x1b[0m";
-var FgCyan = "\x1b[36m";
-var FgGreen = "\x1b[32m";
-var FgMagenta = "\x1b[35m";
+    case 'tweets':
+        twitterFn();
+        break;
+    case 'spotify':
+        spotifyFn();
+        break;
+    default:
+        break;
+}
 
-// Turns user search into string for placement in urls (OMDB & Spotify)
-// i === 3 to capture the first meaningful userinput
-for (var i = 3; i < entireCmdLine.length; i++) {
-    if (i > 3 && i < entireCmdLine.length) {
-        findThis = findThis + "+" + entireCmdLine[i];
-    } else {
-        findThis += entireCmdLine[i];
-    };
-};
+// the variables are createda and now have to get the functionality
+//=====================================================================
+//Pseudo code
 
-// Searches for results based on userinput 
-function liriBot() {
+// 1. Needs to create   funciton that will get all the executable logic to communicate to tweeter.
+// 2. Needs to create   funciton that will get all the executable logic to communicate to Spotify.
+// 3. Needs to create   funciton that will get all the executable logic to communicate to OMDB.
 
-    switch (search) {
-        case "my-tweets":
-            var params = { screen_name: 'CoderChicago' };
-            var logIt1 = search.concat(findThis + ", ")
-            console.log("Searching " + FgCyan + "Twitter" + FgWhite + " for your tweets!\n");
-            client.get('statuses/user_timeline', params, function (error, tweets, response) {
-                if (!error) {
-                    console.log(FgCyan + "----------Twitter Last 20 Tweets!----------\n" + FgWhite);
-                    for (i = 0; i < tweets.length && i < 20; i++) {
-                        console.log(FgCyan + (i + 1) + ": " + FgWhite + tweets[i].text);
-                        console.log(FgCyan + "-- " + tweets[i].created_at.slice(0, 20) + FgWhite);
-                        var logIt2 = logIt1 + tweets[i].created_at.slice(0, 20) + " " + tweets[i].text;
-                        fs.appendFile("log.txt", logIt2 + "\n", function (err) {
-                            if (err) {
-                                return console.log(err);
-                            }
-                            logIt2 = "";
-                        });
+function movie() {
+    // search the omdb for user movies , if there is no movie selection have to rerturn Mr Nobody
+    var quryURL = "http://www.omdbapi.com/?apikey=trilogy&y=&plot=short&t=";
+    var search = process.argv[3] || 'Mr+Nobody';
+    quryURL += search;
 
-                    };
-                    console.log(FgCyan + "\n-------------------------------------------" + FgWhite);
-                    console.log("\nlog.txt was updated!\n");
-                };
-            });
-            break;
+    console.log(quryURL);
+    request(quryURL, function (err, response, body) {
+        if (!err && response.statusCode === 200) {
+            var movieResponse = JSON.parse(body);
+            console.log(`title, ${movieResponse.Title}`);
+            console.log(`title, ${movieResponse.Plot}`);
+            console.log(`title, ${movieResponse.Rating}`);
+        }
+    })
+}
 
-        case "spotify-this-song":
-            var logIt = search.concat("," + findThis)
+function twitterFn() {
+    var search = process.argv[3] || 'Chicago';
+    client.get("search/tweets", { q: search }, function (error, tweets, response) {
+        for (var i = 0; i < 10; i++) {
+            console.log(`${i + 1} ${tweets.statuses[i].created_at} => ${tweets.statuses[i].text}`)
+        }
+    })
 
-            if (findThis === "") {
-                findThis = "The+sign+ace+of+base" //  If the user doesn't type a song in, the program will output data for the song "The Sign."
-                logIt += "The Sign Ace of Base";
-            }
 
-            console.log("Searching " + FgGreen + "Spotify..." + FgWhite + "\n");
 
-            spotify.search({ type: 'track', query: findThis }, function (err, data) {
-                if (err) {
-                    return console.log('Error occurred: ' + err);
-                }
+}
 
-                console.log(FgGreen + "----------Spotify Search Results!----------\n" + FgWhite);
-                console.log(FgGreen + 'Artist(s): ' + FgWhite + data.tracks.items[0].artists[0].name);
-                console.log(FgGreen + 'Song: ' + FgWhite + data.tracks.items[0].name);
-                console.log(FgGreen + 'Album: ' + FgWhite + data.tracks.items[0].album.name);
-                console.log(terminalLink(FgGreen + 'Preview:' + FgWhite, data.tracks.items[0].artists[0].external_urls.spotify));
-                console.log(FgGreen + "\n-------------------------------------------" + FgWhite);
-                fs.appendFile("log.txt", logIt + "\n", function (err) {
-                    if (err) {
-                        return console.log(err);
-                    }
-                    console.log("\nlog.txt was updated!\n");
-                });
-            });
-            break;
+function spotifyFn() {
+    var search = process.argv[3] || 'Chicago';
+    spotify.search({ type: 'track', query: search, limit: 5}, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
 
-        case "movie-this":
+        for (var i=0;i < data.tracks.items.length; i++) { 
+            console.log(data.tracks.items[i].name);
+        }
+    });
+}
 
-            if (findThis === "") {
-                findThis = "Mr.+Nobody" //  If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-            }
 
-            var queryUrl = "http://www.omdbapi.com/?t=" + findThis + "&y=&plot=short&apikey=" + keys.omdb.key;
-            var logIt = search.concat("," + findThis)
 
-            console.log("Searching " + FgBlue + "OMDB..." + FgWhite + "\n")
-
-            request(queryUrl, function (error, response, body) {
-
-                if (error) {
-                    console.log(error);
-                } else if (!error && response.statusCode === 200 && JSON.parse(body).Ratings !== undefined) {
-                    console.log(FgBlue + "----------Your Movie Search Results----------\n" + FgWhite);
-                    console.log(FgBlue + "Year: " + FgWhite + JSON.parse(body).Year);
-                    console.log(FgBlue + "Title: " + FgWhite + JSON.parse(body).Title);
-                    console.log(FgBlue + "Actors: " + FgWhite + JSON.parse(body).Actors);
-                    console.log(FgBlue + "Country: " + FgWhite + JSON.parse(body).Country);
-                    console.log(FgBlue + "Language: " + FgWhite + JSON.parse(body).Language);
-                    console.log(FgBlue + "IMDB Rating: " + FgWhite + JSON.parse(body).imdbRating);
-                    console.log(FgBlue + "Rotten Tomatoes Rating: " + FgWhite + JSON.parse(body).Ratings[1].Value);
-                    console.log(FgBlue + "Plot: " + FgWhite + JSON.parse(body).Plot);
-                    console.log(FgBlue + "\n---------------------------------------------" + FgWhite);
-                    fs.appendFile("log.txt", logIt + "\n", function (err) {
-                        if (err) {
-                            return console.log(err);
-                        }
-                        console.log("\nlog.txt was updated!\n");
-                    });
-                } else {
-                    console.log("That movie does not exist in the " + FgBlue + "OMDB..." + FgWhite + "\n")
-                }
-
-            });
-            break;
-
-        case "do-what-it-says":
-
-            fs.readFile("random.txt", "utf8", function (error, data) {
-                // Break the string down by comma separation and store the contents into the output array.
-                var dataArr = data.split(",");
-                if (error) {
-                    return console.log(error);
-                }
-                search = dataArr[0];
-                findThis = dataArr[1];
-                liriBot();
-            });
-
-            break;
-
-        default:
-            console.log("Not a valid input. Please use the following options: " + FgGreen + "\n * spotify-this-song " + FgCyan + "\n * my-tweets " + FgBlue + "\n * movie-this " + FgMagenta + "\n * do-what-it-says" + FgWhite)
-            break;
-    };
-};
-
-liriBot();
+// 4. The functions have to requre the info from the websites
+// 5. Have to create requre connection with the websites by using the require packages
+// 6. Needs to create an API string that searches the OMDB by the URL
+// 7. Have to console.log all the reguests after they are parsed from JSON.
